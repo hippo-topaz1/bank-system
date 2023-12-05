fa99ff238d0b5f09d9c2657dbeca2c1d990a81ee
Markus Müller <152977588+hippo-topaz1@users.noreply.github.com>
2023-12-05 20:06:01 +0700

    Create transaction.h

---
+++ transaction.h
@@ -0,0 +1,9 @@
+#ifndef TRANSACTION_H
+#define TRANSACTION_H
+
+// Define the Transaction class with necessary attributes and methods
+class Transaction {
+    // Add member variables and functions as needed
+};
+
+#endif // TRANSACTION_H
