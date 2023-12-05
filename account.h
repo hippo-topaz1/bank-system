97e16e0d33bb14346291bdb8941ab1d7adf81f38
Markus Müller <152977588+hippo-topaz1@users.noreply.github.com>
2023-12-05 20:05:45 +0700

    Create account.h

---
+++ account.h
@@ -0,0 +1,9 @@
+#ifndef ACCOUNT_H
+#define ACCOUNT_H
+
+// Define the Account class with necessary attributes and methods
+class Account {
+    // Add member variables and functions as needed
+};
+
+#endif // ACCOUNT_H
