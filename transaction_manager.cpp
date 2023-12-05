59b1e1f4b5ac2f01551518712e20e4c726506663
Markus Müller <152977588+hippo-topaz1@users.noreply.github.com>
2023-12-05 20:04:25 +0700

    Create transaction_manager.cpp

---
+++ transaction_manager.cpp
@@ -0,0 +1,6 @@
+#include "transaction_manager.h"
+
+void TransactionManager::makeTransaction(const Transaction& transaction) {
+    // Implement the logic to process a transaction and add it to the vector
+}
+// Implement other transaction management functions
