969deecebd1d746874d90c0d30cf44e4342cac5d
Markus Müller <152977588+hippo-topaz1@users.noreply.github.com>
2023-12-05 20:03:31 +0700

    Create customer_manager.cpp

---
+++ customer_manager.cpp
@@ -0,0 +1,6 @@
+#include "customer_manager.h"
+
+void CustomerManager::addCustomer(const Customer& customer) {
+    // Implement the logic to add a customer to the vector
+}
+// Implement other customer management functions
