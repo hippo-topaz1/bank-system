95a5b3db2bc0d0bd281c0e2c593e9f5bfcd6be31
Markus Müller <152977588+hippo-topaz1@users.noreply.github.com>
2023-12-05 20:04:54 +0700

    Create customer.h

---
+++ customer.h
@@ -0,0 +1,9 @@
+#ifndef CUSTOMER_H
+#define CUSTOMER_H
+
+// Define the Customer class with necessary attributes and methods
+class Customer {
+    // Add member variables and functions as needed
+};
+
+#endif // CUSTOMER_H
