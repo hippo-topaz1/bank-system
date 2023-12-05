09818cecb8ee97944409fecaebf410d955edf62f
Markus Müller <152977588+hippo-topaz1@users.noreply.github.com>
2023-12-05 20:04:00 +0700

    Create account_manager.cpp

---
+++ account_manager.cpp
@@ -0,0 +1,6 @@
+#include "account_manager.h"
+
+void AccountManager::addAccount(const Account& account) {
+    // Implement the logic to add an account to the vector
+}
+// Implement other account management functions
